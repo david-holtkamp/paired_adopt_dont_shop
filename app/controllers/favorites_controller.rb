9c2ba7b1b544c886a62e122536f1856b0da76dad
David Atkins <atki1080@gmail.com>
2020-02-06 08:10:37 -0700

    Move logic to model

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,9 @@
 class FavoritesController < ApplicationController
-  include ActionView::Helpers::TextHelper
 
   def update
     pet = Pet.find(params[:pet_id])
-    pet_id_str = pet.id.to_s
-    session[:favorites] ||= Hash.new #Array.new
-    session[:favorites][pet_id_str] ||= true
-
-    # session[:favorites].index(pet_id_str) || pet_id_str
-
+    favorites.add_pet(pet.id)
+    session[:favorites] = favorites.contents
     flash[:notice] = "#{pet.name} has been added to favorites!"
     redirect_to "/pets/#{params[:pet_id]}"
   end
--- app/models/favorites.rb
+++ app/models/favorites.rb
@@ -2,7 +2,11 @@ class Favorites
   attr_reader :contents
 
   def initialize(initial_contents)
-    @contents = initial_contents || Hash.new(0)
+    @contents = initial_contents || Array.new
+  end
+
+  def add_pet(id)
+    @contents << id if !@contents.include?(id)
   end
 
   def total_count
