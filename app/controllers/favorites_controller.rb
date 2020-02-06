f735d5b2aa3842acd69fc0a8b426265b7dca50b6
David Atkins <atki1080@gmail.com>
2020-02-06 10:05:20 -0700

    Change contents in session[:favorites] to hash

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,8 @@ class FavoritesController < ApplicationController
 
   def update
     pet = Pet.find(params[:pet_id])
-    favorites.add_pet(pet.id)
+    # favorites.add_pet(pet.id)
+    favorites.add_pet({id: pet.id, info: {name: pet.name, image: pet.image}})
     session[:favorites] = favorites.contents
     flash[:notice] = "#{pet.name} has been added to favorites!"
     redirect_to "/pets/#{params[:pet_id]}"
--- app/models/favorites.rb
+++ app/models/favorites.rb
@@ -2,11 +2,12 @@ class Favorites
   attr_reader :contents
 
   def initialize(initial_contents)
-    @contents = initial_contents || Array.new
+    @contents = initial_contents || Hash.new
   end
 
-  def add_pet(id)
-    @contents << id if !@contents.include?(id)
+  def add_pet(pet_info)
+    # @contents << id if !@contents.include?(id)
+    @contents[pet_info[:id]] ||= pet_info[:info]
   end
 
   def total_count
