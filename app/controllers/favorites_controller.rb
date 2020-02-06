965ab920edb54cb3739c476b16c06a4d51074247
David Atkins <atki1080@gmail.com>
2020-02-06 11:14:16 -0700

    Remove comment

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,6 @@ class FavoritesController < ApplicationController
 
   def update
     pet = Pet.find(params[:pet_id])
-    # favorites.add_pet(pet.id)
     favorites.add_pet({id: pet.id, info: {name: pet.name, image: pet.image}})
     session[:favorites] = favorites.contents
     flash[:notice] = "#{pet.name} has been added to favorites!"
