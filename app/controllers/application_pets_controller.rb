bfb2e544fcf68b57894a2a4400b7743289251eb1
David Atkins <atki1080@gmail.com>
2020-02-09 20:15:28 -0700

    Restrict approving applications to adoptable pets only

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationPetsController < ApplicationController
   def update
     application_pet = ApplicationPet.find_by(application_id: params[:application_id], pet_id: params[:pet_id])
-    application_pet.update(approved: true)
-    application_pet.pet.update(status: "Pending")
+    unless application_pet.pet.status == "Pending"
+      application_pet.update(approved: true)
+      application_pet.pet.update(status: "Pending")
+    else
+      flash[:notice] = "#{application_pet.pet.name} is already pending adoption."
+    end
     redirect_to "/pets/#{application_pet.pet.id}"
   end
 end
