a70ab03ce94b5b0453ba54ebe5276fcaf283f44f
David Atkins <atki1080@gmail.com>
2020-02-09 21:12:09 -0700

    Add cancel approval logic

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationPetsController < ApplicationController
   def update
     application_pet = ApplicationPet.find_by(application_id: params[:application_id], pet_id: params[:pet_id])
-    unless application_pet.pet.status == "Pending"
-      application_pet.update(approved: true)
-      application_pet.pet.update(status: "Pending")
-    else
-      flash[:notice] = "#{application_pet.pet.name} is already pending adoption."
+    if params[:approval] == 'approve'
+      unless application_pet.pet.status == "Pending"
+        application_pet.update(approved: true)
+        application_pet.pet.update(status: "Pending")
+      else
+        flash[:notice] = "#{application_pet.pet.name} is already pending adoption."
+      end
+      redirect_to "/pets/#{application_pet.pet.id}"
+    elsif params[:approval] == 'cancel'
+      application_pet.update(approved: false)
+      application_pet.pet.update(status: "Adoptable")
+      redirect_back(fallback_location: "/pets") 
     end
-    redirect_to "/pets/#{application_pet.pet.id}"
   end
 end
