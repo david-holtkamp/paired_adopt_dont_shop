3ca9c98ed6ae3546044f02097fde38edb654b491
David Holtkamp <david.hholtkamp@gmail.com>
2020-02-10 18:15:43 -0700

    Add dependent destroy
    Co-Authored-By: David Atkins <d-atkins@users.noreply.github.com>

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,7 +1,7 @@
 class Pet < ApplicationRecord
   validates_presence_of :image, :name, :description, :age, :sex, :status
   belongs_to :shelter
-  has_many :application_pets
+  has_many :application_pets, dependent: :destroy
   has_many :applications, through: :application_pets
 
   def self.sort_by_status
