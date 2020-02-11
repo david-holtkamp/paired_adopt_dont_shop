f8b76e7d145be6c1fdddb3227f89cda2d7f72f93
David Holtkamp <david.hholtkamp@gmail.com>
2020-02-11 11:18:39 -0700

    Create dependency to shelter for destroy
    Co-Authored-By: David Atkins <d-atkins@users.noreply.github.com>

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,7 +1,7 @@
 class Shelter < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   has_many :pets, dependent: :destroy
-  has_many :reviews
+  has_many :reviews, dependent: :destroy 
 
   def pet_count(status = 'all')
     return pets.count if status == 'all'
