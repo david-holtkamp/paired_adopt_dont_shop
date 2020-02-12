89fc6fc1ccbded2e1aa61b0b5e1ec1c83c4a87e8
David Holtkamp <david.hholtkamp@gmail.com>
2020-02-11 18:18:13 -0700

    most functionality implemented but number of applications for a shelter
    Co-Authored-By: David Atkins <d-atkins@users.noreply.github.com>

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,10 +1,19 @@
 class Shelter < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   has_many :pets, dependent: :destroy
-  has_many :reviews, dependent: :destroy 
+  has_many :reviews, dependent: :destroy
+
 
   def pet_count(status = 'all')
     return pets.count if status == 'all'
     return pets.where(status: status).count if status != 'all'
+  end
+
+  def average_review_rating
+    reviews.average(:rating).to_f
+  end
+
+  def applicant_count
+    
   end
 end
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -57,5 +57,81 @@ RSpec.describe Shelter, type: :model do
       expect(shelter_a.pet_count).to eq(0)
       expect(cdp.pet_count).to eq(2)
     end
+
+    it "average_review_rating" do
+      shelter_1 = Shelter.create!(
+        name: "Shelter A",
+        address: "123 Fake St",
+        city: "Las Vegas",
+        state: "WY",
+        zip: "12345"
+      )
+
+      img_review = Review.create!(
+        title: "omg!!!",
+        rating: 5,
+        content: "pets super amazin!!",
+        image: "https://cdn0.wideopenpets.com/wp-content/uploads/2016/04/valentines-8.jpg",
+        shelter: shelter_1
+      )
+      no_img_review = Review.create!(
+        title: "eh",
+        rating: 3,
+        content: "it's ok I guess",
+        shelter: shelter_1
+      )
+      expect(shelter_1.average_review_rating).to eq(4)
+    end
+
+    it "applicant_count" do
+      shelter_1 = Shelter.create!(
+        name: "Shelter A",
+        address: "123 Fake St",
+        city: "Las Vegas",
+        state: "WY",
+        zip: "12345")
+
+      pet_1 = Pet.create(
+        image: "https://i.pinimg.com/originals/a9/cf/64/a9cf6473ca327409108ab02d15cc06b0.jpg",
+        name: "Snoopy",
+        description: "beagle pup eh",
+        age: "6 months old",
+        sex: "male",
+        shelter: shelter_1)
+
+      pet_2 = Pet.create(
+        image: "https://upload.wikimedia.org/wikipedia/commons/2/2b/WelshCorgi.jpeg",
+        name: "Nana",
+        description: "super cute dog in need of home",
+        age: "4 years old",
+        sex: "female",
+        status: "Pending",
+        shelter: shelter_1)
+
+      app_1 = Application.create({
+        name: "David H",
+        address: "1234 julian st.",
+        city: "Denver",
+        state: "CO",
+        zip: "80211",
+        phone_number: "303-465-1112",
+        description: "I like dogs."
+      })
+
+      app_2 = Application.create({
+        name: "David A",
+        address: "1234 elm st.",
+        city: "Denver",
+        state: "CO",
+        zip: "80212",
+        phone_number: "303-465-4443",
+        description: "I like dog."
+      })
+
+      app_1.pets << [pet_1, pet_2]
+      app_2.pets << pet_1
+
+      expect(shelter_1.applicant_count).to eq(2)
+    end
   end
 end
