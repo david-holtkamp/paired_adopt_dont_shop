336e300bd999f1d1cdacd0abaf74def624e06210
David Atkins <atki1080@gmail.com>
2020-02-12 20:48:26 -0700

    Add helper for checking links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :favorites
+  helper_method :favorites, :is_invalid?, :linkify
 
   def favorites
     @favorites ||= Favorites.new(session[:favorites])
+  end
+
+  def is_invalid?(path)
+    path.index("http://") != 0 || path.index("https://") != 0
+  end
+
+  def linkify(path)
+    path.prepend("https://")
   end
 end
